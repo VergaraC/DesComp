db64cdd84ec195f409af92ff00f75e708c5f3498
VergaraC <victorvaac@al.insper.edu.br>
2021-10-19 23:40:32 -0300

    Work fdp n vai

--- Projeto2/Projeto2.vhd
+++ Projeto2/Projeto2.vhd
@@ -1,5 +1,6 @@
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
 entity Projeto2 is
   -- Total de bits das entradas e saidas
@@ -12,7 +13,9 @@ entity Projeto2 is
     CLOCK_50 : in std_logic;
 	 Wr3: in std_logic;
 	 Seletor_ULA: in std_logic;
-	 
+	 A : std_logic_vector(larguraDados-1 downto 0);
+	 B : std_logic_vector(larguraDados-1 downto 0);
+    OutULA : std_logic_vector(larguraDados-1 downto 0)
 	 
   );
 end entity;
@@ -20,14 +23,15 @@ end entity;
 
 architecture arquitetura of Projeto2 is
 	signal CLK : std_logic;
-	signal Instruction : std_logic(larguraDados-1 downto 0);
-	signal PC_ROM : std_logic(larguraDados-1 downto 0);
+	signal Instruction : std_logic_vector(larguraDados-1 downto 0);
+	signal PC_ROM : std_logic_vector(larguraDados-1 downto 0);
 	
-	signal SomadorPC : std_logic(larguraDados-1 downto 0);
+	signal SomadorPC : std_logic_vector(larguraDados-1 downto 0);
   
-	signal BancoULA_A : std_logic(larguraDados-1 downto 0);
-	signal BancoULA_B : std_logic(larguraDados-1 downto 0);
-	signal ULA_Out : std_logic(larguraDados-1 downto 0);
+	signal BancoULA_A : std_logic_vector(larguraDados-1 downto 0);
+	signal BancoULA_B : std_logic_vector(larguraDados-1 downto 0);
+	signal ULA_Out : std_logic_vector(larguraDados-1 downto 0);
+
 
 begin
 
@@ -46,18 +50,9 @@ somaUm :  entity work.somaConstante  generic map (larguraDados => larguraDados, 
 ULA1 : entity work.ULASomaSub  generic map(larguraDados => larguraDados)
           port map (entradaA => BancoULA_A, entradaB => BancoULA_B, saida => ULA_Out, seletor => Seletor_ULA);
 
-			 Banco_Registradores : entity work.bancoRegistradoresArqRegMem 
-		generic map (larguraDados => larguraDados, larguraEndBancoRegs => larguraAddrBanco)
-		port map ( 
-			  clk => CLK,
-			  enderecoA => Instruction(15 downto 11),    
-			  enderecoB => Instruction(20 downto 16),   
-			  enderecoC => Instruction(25 downto 21),
-			  dadoEscritaC => ULA_Out,
-			  escreveC => Wr_R3,
-			  saidaA => BancoULA_A,
-			  saidaB => BancoULA_B
-		 );
+Banco_Registradores : entity work.bancoRegistradoresArqRegMem  generic map (larguraDados => larguraDados, larguraEndBancoRegs => larguraAddrBanco)
+			 port map ( clk => CLK, A => Instruction(15 downto 11), B => Instruction(20 downto 16),C => Instruction(25 downto 21), DadosC => ULA_Out,
+				 FlagC => Wr_R3, A_Out => BancoULA_A, B_Out => BancoULA_B);
 		 
 ROM1 : entity work.MemoriaROM   generic map (dataWidth => larguraDadosROM, addrWidth => larguraAddrROM)
 		 port map (
@@ -65,8 +60,9 @@ ROM1 : entity work.MemoriaROM   generic map (dataWidth => larguraDadosROM, addrW
 			 Dado => Instruction
 		 );
 		 
---end generate;
-
+A <= BancoULA_A;
+B <= BancoULA_B;
+OutULA <= ULA_Out;
 
 end architecture;
 
--- Projeto2/bancoRegistradoresArqRegMem.vhd
+++ Projeto2/bancoRegistradoresArqRegMem.vhd
@@ -1,41 +1,57 @@
+-- Baseado no apendice C (Register Files) do COD (Patterson & Hennessy).
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
-
-entity bancoRegistradoresArqRegMem is
-    generic
-    (
-        larguraDados        : natural := 8;
-        larguraEndBancoRegs : natural := 3   --Resulta em 2^3=8 posicoes
+entity bancoRegistradores is
+    generic(
+        larguraDados        : natural := 32;
+        larguraEndBancoRegs : natural := 5   
     );
-
--- Leitura e escrita de um registrador.
-    port
-    (
-        clk        : in std_logic;
-        endereco       : in std_logic_vector((larguraEndBancoRegs-1) downto 0);
-        dadoEscrita    : in std_logic_vector((larguraDados-1) downto 0);
-        habilitaEscrita: in std_logic := '0';
-        saida          : out std_logic_vector((larguraDados -1) downto 0)
+    port (
+        
+        A       : in std_logic_vector((larguraEndBancoRegs-1) downto 0);
+        B       : in std_logic_vector((larguraEndBancoRegs-1) downto 0);
+        C       : in std_logic_vector((larguraEndBancoRegs-1) downto 0);
+        DadosC    : in std_logic_vector((larguraDados-1) downto 0);
+        FlagC        : in std_logic := '0';
+        A_Out          : out std_logic_vector((larguraDados -1) downto 0);
+        B_Out          : out std_logic_vector((larguraDados -1) downto 0);
+		  clk        : in std_logic
     );
 end entity;
 
-architecture comportamento of bancoRegistradoresArqRegMem is
+architecture comportamento of bancoRegistradores is
 
     subtype palavra_t is std_logic_vector((larguraDados-1) downto 0);
     type memoria_t is array(2**larguraEndBancoRegs-1 downto 0) of palavra_t;
 
-    -- Declaracao dos registradores:
-    shared variable registrador : memoria_t;
+function initMemory
+        return memoria_t is variable tmp : memoria_t := (others => (others => '0'));
+  begin
+        -- Inicializa os endereços:
+        tmp(0) := x"00000000";  -- Nao deve ter efeito.
+        tmp(8)  := "00000000000000000000000000000000";  -- $t0 = 0x00
+        tmp(9)  := "00000000000000000000000000000000"; -- $t1 = 0x0A
+        tmp(10) := "00000000000000000000000000000000";  -- $t2 = 0x0B
+        tmp(11) := "00000000000000000000000000000000";  -- $t3 = 0x0C
+        tmp(12) := "00000000000000000000000000000000";  -- $t4 = 0x0D
+        tmp(13) := "00000000000000000000000000000000";  -- $t5 = 0x16
+        return tmp;
+    end initMemory;
 
+    -- Declaracao dos registradores:
+    shared variable registrador : memoria_t := initMemory;
+    constant zero : std_logic_vector(larguraDados-1 downto 0) := (others => '0');
 begin
     process(clk) is
     begin
         if (rising_edge(clk)) then
-            if (habilitaEscrita = '1') then
-                registrador(to_integer(unsigned(endereco))) := dadoEscrita;
+            if (FlagC = '1') then
+                registrador(to_integer(unsigned(C))) := DadosC;
             end if;
         end if;
     end process;
-    saida <= registrador(to_integer(unsigned(endereco)));
+    -- Se endereco = 0 : retorna ZERO
+    B_Out <= zero when to_integer(unsigned(B)) = to_integer(unsigned(zero)) else registrador(to_integer(unsigned(B)));
+    A_Out <= zero when to_integer(unsigned(A)) = to_integer(unsigned(zero)) else registrador(to_integer(unsigned(A)));
 end architecture;
